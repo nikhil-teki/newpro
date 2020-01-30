2044b43770d60e792780846d1ec3b22931a0db6e
nikhil-teki <git@github.com>
2020-01-30 22:17:20 +0530

    30-01-2020 10:17 PM nkl

---
+++ my-app/src/main/java/com/nikhil/app/App.java
@@ -0,0 +1,13 @@
+package com.nikhil.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "OM SRI SAI RAM" );
+    }
+}
